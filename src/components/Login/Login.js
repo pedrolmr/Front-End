e598745a79be420b9e4788a595ecd63ca81e8299
jurusteve <jstevemu17@gmail.com>
2019-10-21 17:07:05 -0400

    added registration form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Form from "./Form";
 import Styled from "styled-components";
 
@@ -9,7 +10,7 @@ function Login() {
       <Form />
 
       <h4>
-        Don't have an account? <a href="/register">Register Here</a>
+        Don't have an account? <Link to="/register">Register Here</Link>
       </h4>
     </LoginPage>
   );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Styled from "styled-components";
+import Form from "../Login/Form";
 
 function Register() {
   return (
-    <div>
+    <RegisterContainer>
       <h1>Register Page</h1>
-    </div>
+      <Form />
+
+      <h4>
+        Already have an account? <Link to="/login">Login Here</Link>
+      </h4>
+    </RegisterContainer>
   );
 }
+
+const RegisterContainer = Styled.div`
+  text-align: center
+`;
 
 export default Register;
